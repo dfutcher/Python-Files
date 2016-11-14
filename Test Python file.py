582832c3e0f3153569885e0cf8960716b9e9a8aa
David Futcher <djf@cranleigh.org>
2016-11-14 21:40:45 +0000

    Add files via upload
    Use of while True and .lower()

---
+++ Test Python file.py
@@ -0,0 +1,11 @@
+#Python Program
+while True:
+    answer = input("do you like my new toy? Y/N ").lower()
+    if answer == "y" :
+        print()
+        print("me too :-)")
+        break
+    else:
+        print()
+        print("Why not")
+        print()
