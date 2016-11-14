7fe8731f32d271f03d6ff60a3c6d51206820a1d0
David Futcher <djf@cranleigh.org>
2016-11-14 23:15:43 +0000

    added further elif options

--- Why.py
+++ Why.py
@@ -5,6 +5,9 @@ bed = "bed"
 sleep = "sleep"
 why = "why"
 stop="stop"
+team = "liverpool"
+town = "manchester"
+dance = "dance"
 print("Hello, my name is Jack\n")
 while True:
     answer = input("What would you like me to do? ").lower()
@@ -26,6 +29,18 @@ while True:
     elif why in words:
         print()
         print ("I don't know why I like why")
+        print()
+    elif team in words:
+        print()
+        print ("Nothing wrong with Liverpool...it's just Manchester Utd are better")
+        print()
+    elif town in words:
+        print()
+        print ("I will only hear good things about Manchester!")
+        print()
+    elif dance in words:
+        print()
+        print ("I learned my best moves from daddy")
         print()
     elif stop in words:
         print()
